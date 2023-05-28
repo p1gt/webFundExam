f8afad514810309ba78adcc9180c54670e542c58
p1gt <ranaranap59@gmail.com>
2023-05-28 19:44:06 +0300

    my web fundamentals exam

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("hii")
+const a = "your shopping cart is empty!!"
+function convert(){
+    document.querySelector(".succ").src = "assets/succulents-2.jpg"
+}
+function deconvert(){
+    document.querySelector(".succ").src = "assets/succulents-1.jpg"
+}
+function destroy(){
+    document.querySelector(".cookies").remove();
+}
